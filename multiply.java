48c538f3c6371113f51b4b2cfb9ece1f3ddfe9e4
Aditi Pandey <aditipandey256@gmail.com>
2021-07-24 23:57:54 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+ublic class multiply {  
+    public static void main(String[] args) {          
+      int a = 65;  
+        int b = 35;  
+        // It will return the sum of a and b.  
+        System.out.println(" product of a and b is = " + (a* b));  
+    }  
+}  
